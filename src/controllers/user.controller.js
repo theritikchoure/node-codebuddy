f77811f40846fc9fffce71b5a22de23fe49a78d1
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2023-06-08 16:37:55 +0530

    feat: Get users with posts number with pagination

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,61 @@ const User = require("../schema/user.schema");
 
 module.exports.getUsersWithPostCount = async (req, res) => {
   try {
-    //TODO: Implement this API
+    // Pagination parameters
+    const pageSize = Number(req.query.limit) || 10; // Number of documents per page
+    const currentPage = Number(req.query.page) || 1; // Current page number
 
-    res.status(200).json({});
+    // Calculate skip value for pagination
+    const skip = (currentPage - 1) * pageSize;
+
+    // Retrieve users with post count
+    const users = await User.aggregate([
+      {
+        $lookup: {
+          from: "posts", // Collection name for posts
+          localField: "_id", // User id field
+          foreignField: "userId", // Post user id field
+          as: "posts", // Array field to store posts
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          posts: { $size: "$posts" },
+        },
+      },
+      { $skip: skip }, // Skip documents
+      { $limit: pageSize }, // Limit documents
+    ]);
+
+    // Retrieve total document count for pagination
+    const totalCount = await User.countDocuments();
+    const totalPages = Math.ceil(totalCount / pageSize);
+    const hasNextPage = currentPage < totalPages;
+
+    // Pagination object
+    const pagination = {
+      totalDocs: totalCount,
+      limit: pageSize,
+      page: currentPage,
+      totalPages,
+      pagingCounter: (currentPage - 1) * pageSize + 1,
+      hasNextPage,
+      hasPrevPage: currentPage > 1,
+      prevPage: currentPage > 1 ? currentPage - 1 : null,
+      nextPage: hasNextPage ? currentPage + 1 : null,
+    };
+
+    // Response object
+    const data = {
+      data: {
+        users,
+        pagination,
+      },
+    };
+
+    res.status(200).json(data);
   } catch (error) {
     res.send({ error: error.message });
   }
